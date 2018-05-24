a65d7e3a33a076a885f5faf92ec65f7644e20cea
Yordan Velásquez <yordan91493@gmail.com>
2018-05-23 20:16:50 -0400

    Primer Commit

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
+  Route::get('/user', function( Request $request ){
+    return $request->user();
+  });
+});
